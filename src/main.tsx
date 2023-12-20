91798c2d3c43dbdee14a96a5dc020e07f926eb64
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-12-20 16:07:49 +0100

    configured scss, aded structure and basic styles and variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/css/styles.css";
+import "./styles/css/main.css";
 import App from "./App.tsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
