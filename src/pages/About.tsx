244759c290fd1cccd4a81e83a828a60fc2f5f9c9
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-12-21 14:06:29 +0100

    created about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,17 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <section className="container min-h-70">
+      <h2>About</h2>
+      <p className="leading-8 mt-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit adipisci
+        laborum officiis quod, nemo, cum veniam laboriosam autem error aut
+        fugiat, voluptatem impedit eius accusantium doloremque minima non
+        placeat. Dolores accusamus fuga quaerat mollitia necessitatibus corporis
+        deleniti fugit est, earum illum id eveniet expedita. Explicabo
+        voluptatum dolor a beatae? Cupiditate?
+      </p>
+    </section>
+  );
 }
 
 export default About;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,jsx,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    minHeight: {
+      70: "70vh",
+    },
+  },
 };
 export const plugins = [];
