9e8102fae8e0cbccdda61f94d7f50949313c4f20
Ljusaj123 <rosana.ljusaj2@gmail.com>
2023-12-22 21:44:26 +0100

    added other cases for global errors

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -16,6 +16,12 @@ function Error() {
       </div>
     );
   }
+  return (
+    <div>
+      <h3>Something went wrong...</h3>
+      <p>{error.message}</p>
+    </div>
+  );
 }
 
 export default Error;
